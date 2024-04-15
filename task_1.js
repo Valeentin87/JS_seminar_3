4c8b1b709efdaa532b88aaa64d5f90e8c5a6773d
Valeentin87 <valeentin87@list.ru>
2024-04-15 18:01:47 +0300

    решение 1 задачи

--- task_1.js
+++ task_1.js
@@ -1,1 +1,7 @@
 // Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
+let thirdDegree = (value) => {
+  return value ** 3;
+};
+console.log(thirdDegree(2));
+console.log(thirdDegree(3));
+console.log('2^3 степени + 3 ^ 3 степени равно: ', thirdDegree(2) + thirdDegree(3));
